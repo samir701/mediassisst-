28ed7f56e632c4ae22300f26b86b726b0f719cf5
samir <samirbrahma2003@gmail.com>
2025-08-01 12:14:40 +0530

    commit

---
+++ FRONTEND/medassist/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './assets/css/App.css';
+
+export default function App() {
+  return (
+    <div className='app'>
+      <p>hellow world</p>
+    </div>
+    
+  )
+}
